ad8a74d4f1c2c838ceefbf32190d4f57efd69913
volos <volosain@mail.ru>
2023-02-23 21:21:59 +0600

    result

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean =  (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1 == beanCat2);
     }
 }
